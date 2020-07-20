31030133ca913cef0f3b88826cf52db979b8597e
Szabó Álmos <53559900+szaboalmos@users.noreply.github.com>
2020-07-20 14:11:15 +0200

    fasz

--- src/pins/Pins.h
+++ src/pins/Pins.h
@@ -1,13 +1,15 @@
+#include <Arduino.h>
+
 #define rotatyEncoder1SW 2
 #define rotatyEncoder1DT 3
 #define rotatyEncoder1CLK 4
-#define rotatyEncoder2SW 11
-#define rotatyEncoder2DT 12
-#define rotatyEncoder2CLK 13
 #define rtcCLK 5
 #define rtcDAT 6
 #define rtcRST 7
 #define relayLED 8
 #define relayINFRA 9
 #define sensorTEMP 10
-#define sensorLIGHT A2
+#define rotatyEncoder2SW 11
+#define rotatyEncoder2DT 12
+#define rotatyEncoder2CLK 13
+#define sensorLIGHT A3
---
+++ src/pins/pinDeclaration.h
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+
+#include "pins.h"
+
+
+void pinDeclaration() {
+    pinMode(relayLED, OUTPUT);
+    pinMode(relayINFRA, OUTPUT);
+}
